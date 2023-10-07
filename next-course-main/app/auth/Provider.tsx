cc1efefb52c819a54b5dd52bfceb26242acee5df
Shukhratbek Uljaev <eyehorus42@gmail.com>
2023-10-07 00:47:53 -0700

    fixed ;

--- next-course-main/app/auth/Provider.tsx
+++ next-course-main/app/auth/Provider.tsx
@@ -1,12 +1,10 @@
-'use client'; 
+"use client";
 
-import React, { ReactNode } from 'react'
-import { SessionProvider } from 'next-auth/react'
+import React, { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
 
 const AuthProvider = ({ children }: { children: ReactNode }) => {
-  return (
-    <SessionProvider>{children}</SessionProvider>
-  )
-}
+  return <SessionProvider>{children}</SessionProvider>;
+};
 
-export default AuthProvider
+export default AuthProvider;
